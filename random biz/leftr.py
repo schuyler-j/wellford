0ef62a9654eaf4666b6a99ee2d714526cf816c9c
schuyler <schuyler@MSI.lan>
2020-11-28 11:48:06 +1030

    progress??

--- random biz/leftr.py
+++ random biz/leftr.py
@@ -7,9 +7,9 @@ l = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 #empty lists
 f = []
 g = []
-
-correct = 0
-
+total = []
+global cor
+cor = 0
 def getran():
     for i in l:
         n = random.randint(0, 1)
@@ -20,55 +20,27 @@ def getran():
         b = g.append(m)
 
 def check():
-    correct = 0
-    i = 0
-    if f[i] == g[i]:
-        correct = correct + 1
-'''
-def check1():
-    i = 1
-    if f[i] == g[i]:
-        correct = correct + 1
-def check2():
-    i = 2
-    if f[i] == g[i]:
-        correct = correct + 1
-def check3():
-    i = 3
-    if f[i] == g[i]:
-        correct = correct + 1
-def check4():
-    i = 4
-    if f[i] == g[i]:
-        correct = correct + 1
-def check5():
-    i = 5
-    if f[i] == g[i]:
-        correct = correct + 1
-def check6():
-    i = 6
-    if f[i] == g[i]:
-        correct = correct + 1
-def check7():
-    if f[i] == g[i]:
-        correct = correct + 1
-'''
+    for i in f:
+        if f[i] == g[i]:
+            global cor 
+            cor = cor + 1
+            total.append(i)
+        else:
+            pass
+    for i in g:
+        if g[i] == f[i]:
+            cor = cor - (i*8)
+            total.append(i)
+        else:
+            pass
 
-
+    print (len(total))
+'''
 getran()
 check()
-'''
-check1()
-check2()
-check3()
-check4()
-check5()
-check6()
-check7()
-'''
-print (correct)
 print (f)
 print (g)
+'''
 
 
 #extended loop (optimise???how?)
@@ -80,18 +52,12 @@ zz = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
         1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
         1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
         1, 2]
-'''
 for i in zz:
     getran()
     if i == 1:
         print(f)
-        time.sleep(2)
+        time.sleep(0.4)
+
     else:
         print(g)
-w!
-'''
-
-
-
-
-
+        check()
